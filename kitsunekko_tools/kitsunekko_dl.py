c00ca62dbc9681f77d310b12fe0592f5026f0383
Ren Tatsumoto <tatsu@autistici.org>
2024-04-15 01:48:21 +0300

    update license

--- kitsunekko_tools/kitsunekko_dl.py
+++ kitsunekko_tools/kitsunekko_dl.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+# Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 import asyncio
 import dataclasses
