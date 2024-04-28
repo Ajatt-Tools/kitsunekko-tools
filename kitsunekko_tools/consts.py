3b021db5adb138bf9d1c853d3104994f87eb519e
Ren Tatsumoto <tatsu@autistici.org>
2024-04-28 19:42:13 +0300

    remove unused

--- kitsunekko_tools/consts.py
+++ kitsunekko_tools/consts.py
@@ -1,12 +1,10 @@
 # Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-import pathlib as _pathlib
 
 PROG_NAME = "kitsunekko-tools"
 SETTINGS_FILE_NAME = f"{PROG_NAME}.toml"
 KITSUNEKKO_DOMAIN_URL = "https://kitsunekko.net"
-REPO = _pathlib.Path(__file__).parent.absolute()
 IGNORE_FILENAME = ".kitsuignore"
 UPDATED_FILENAME = ".updated"
 
