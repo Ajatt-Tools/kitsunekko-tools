0ad507033909551b42466b7b2544e26548c37d40
Ren Tatsumoto <tatsu@autistici.org>
2024-04-28 23:19:51 +0300

    add format script

---
+++ format.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Formatting $PWD"
+
+readarray -t FILES <<<"$(git ls-files | grep -P '\.py$')"
+readonly -a FILES
+
+pyupgrade \
+	--py311-plus \
+	"${FILES[@]}"
+black \
+	"${FILES[@]}"
