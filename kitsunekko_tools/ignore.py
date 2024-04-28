845633e9bb864b5ea6719249bbf545515df8f489
Ren Tatsumoto <tatsu@autistici.org>
2024-04-28 07:18:56 +0300

    update ignore module
    store patterns in a dictonary to maintain order
    
    save to disk separately from appending patterns

--- kitsunekko_tools/ignore.py
+++ kitsunekko_tools/ignore.py
@@ -3,13 +3,11 @@
 
 import dataclasses
 import fnmatch
-import os.path
 import pathlib
-import sys
 import typing
 
 from kitsunekko_tools.common import KitsuException
-from kitsunekko_tools.config import get_config, KitsuConfig
+from kitsunekko_tools.config import KitsuConfig, get_config
 from kitsunekko_tools.consts import *
 
 
@@ -21,16 +19,18 @@ class IgnoreListException(KitsuException):
 class IgnoreList:
     _config: KitsuConfig
     _ignore_filepath: pathlib.Path
-    _patterns: set[str]
+    _patterns: dict[str, None]
+    _is_dirty: bool
 
     def __init__(self, config: KitsuConfig | None = None):
         self._config = config or get_config().data
         self._ignore_filepath = pathlib.Path(self._config.destination) / IGNORE_FILENAME
-        self._patterns = set()
+        self._patterns = {}
+        self._is_dirty = False
         self._config.raise_for_destination()
         try:
             with open(self._ignore_filepath, encoding="utf8") as f:
-                self._patterns.update(filter(bool, map(str.strip, f.read().splitlines())))
+                self._patterns.update(dict.fromkeys(filter(bool, map(str.strip, f.read().splitlines()))))
         except FileNotFoundError:
             pass
 
@@ -38,28 +38,45 @@ class IgnoreList:
     def ignore_filepath(self) -> pathlib.Path:
         return self._ignore_filepath
 
+    def _pattern_from_path(self, file_path: pathlib.Path) -> str:
+        return str(file_path.relative_to(self._config.destination))
+
     def is_matching(self, file_path: pathlib.Path) -> bool:
-        path_dest_stripped = str(file_path.relative_to(self._config.destination))
+        path_dest_stripped = self._pattern_from_path(file_path)
         if path_dest_stripped in self._patterns:
             # try without wildcards first.
             return True
         return any(fnmatch.fnmatch(path_dest_stripped, pattern) for pattern in self._patterns)
 
-    def patterns(self) -> set[str]:
+    def patterns(self) -> typing.Iterable[str]:
         """
         Return all known ignore patterns.
         """
-        return self._patterns
+        return self._patterns.keys()
 
     def add(self, pattern: str):
         """
         Add a new ignore pattern to the list.
         """
-        self._patterns.add(pattern)
-        self._ignore_filepath.write_text("\n".join(sorted(self._patterns)))
+        self._patterns[pattern] = None
+        self._is_dirty = True
+
+    def add_file(self, file_path: pathlib.Path) -> None:
+        """
+        Add file to the list, as relative path.
+        """
+        return self.add(self._pattern_from_path(file_path))
 
     def path(self) -> pathlib.Path:
         """
         Return path to ignore file.
         """
         return self._ignore_filepath
+
+    def commit(self) -> None:
+        """
+        Save ignore file to disk.
+        """
+        if not self._is_dirty:
+            return
+        self._ignore_filepath.write_text("\n".join(self._patterns))
