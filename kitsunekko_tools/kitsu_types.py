9d403d731cce05dd512db9b8d071b58ac561ccad
Ren Tatsumoto <tatsu@autistici.org>
2024-05-01 00:40:36 +0300

    ensure comparisons between objects of the same type

--- kitsunekko_tools/kitsu_types.py
+++ kitsunekko_tools/kitsu_types.py
@@ -1,8 +1,12 @@
 # Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-
 import datetime
 import typing
+
+
+class CompareError(ValueError):
+    def __init__(self, obj1, obj2):
+        super().__init__(f"Can't compare type {type(obj1).__name__} to type {type(obj2).__name__}")
 
 
 class AnimeDir(typing.NamedTuple):
@@ -10,10 +14,12 @@ class AnimeDir(typing.NamedTuple):
     show_name: str  # name of the anime
     mod_timestamp: datetime.datetime  # last modified
 
-    def __eq__(self, other: typing.Self) -> bool:
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, AnimeDir):
+            raise CompareError(self, other)
         return self.show_name == other.show_name
 
-    def __ne__(self, other: typing.Self) -> bool:
+    def __ne__(self, other: object) -> bool:
         return not self.__eq__(other)
 
     def __hash__(self) -> int:
@@ -26,10 +32,12 @@ class SubtitleFile(typing.NamedTuple):
     file_name: str  # name of the subtitle file
     mod_timestamp: datetime.datetime  # last modified
 
-    def __eq__(self, other: typing.Self) -> bool:
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, SubtitleFile):
+            raise CompareError(self, other)
         return self.show_name == other.show_name and self.file_name == other.file_name
 
-    def __ne__(self, other: typing.Self) -> bool:
+    def __ne__(self, other: object) -> bool:
         return not self.__eq__(other)
 
     def __hash__(self) -> int:
