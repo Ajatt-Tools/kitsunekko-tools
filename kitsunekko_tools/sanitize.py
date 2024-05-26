4aed227c547de356ae818baf2a6d9efa0f0578b7
Ren Tatsumoto <tatsu@autistici.org>
2024-05-26 09:14:10 +0300

    add merging logic

--- kitsunekko_tools/sanitize.py
+++ kitsunekko_tools/sanitize.py
@@ -2,26 +2,83 @@
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 import pathlib
 
+from kitsunekko_tools.api_access.root_directory import KitsuDirectoryMeta, EntryId
 from kitsunekko_tools.common import fs_name_strip
 from kitsunekko_tools.config import KitsuConfig
+from kitsunekko_tools.consts import INFO_FILENAME, IGNORE_FILENAME
+
+SKIP_FILES = (IGNORE_FILENAME, INFO_FILENAME)
 
 
 def move_files(old_dir: pathlib.Path, new_dir: pathlib.Path) -> None:
     for entry in old_dir.iterdir():
+        if entry.name in SKIP_FILES:
+            continue
         new_path = new_dir / entry.relative_to(old_dir)
         if new_path.exists():
-            raise RuntimeError(f"already exists: {new_path}")
-        entry.rename(new_path)
+            entry.unlink()
+        else:
+            entry.rename(new_path)
 
 
-def sanitize_directories(config: KitsuConfig) -> None:
+def nuke_dir(directory: pathlib.Path) -> None:
+    (directory / INFO_FILENAME).unlink(missing_ok=True)
+    directory.rmdir()
+
+
+def rename_badly_named_directories(config: KitsuConfig) -> None:
     for directory in config.destination.iterdir():
+        if directory.name in SKIP_FILES:
+            continue
         sanitized_name = fs_name_strip(directory.name)
         if sanitized_name == directory.name:
             continue
         new_dir = directory.parent / sanitized_name
+        print(f"moving '{directory}' to '{new_dir}'")
         if new_dir.exists():
             move_files(directory, new_dir)
-            directory.rmdir()
+            nuke_dir(directory)
         else:
             directory.rename(new_dir)
+
+
+def merge_directories(config: KitsuConfig) -> None:
+    id2master: dict[EntryId, KitsuDirectoryMeta] = {}
+    name2id: dict[str, EntryId] = {}
+
+    for directory in config.destination.iterdir():
+        if directory.name in SKIP_FILES:
+            continue
+        try:
+            with open(directory / INFO_FILENAME, encoding="utf-8") as f:
+                meta = KitsuDirectoryMeta.from_local_file(f, dir_path=directory)
+        except FileNotFoundError:
+            continue
+
+        if meta.entry_id not in id2master:
+            id2master[meta.entry_id] = meta
+        else:
+            id2master[meta.entry_id] = meta = max(meta, id2master[meta.entry_id], key=lambda d: d.last_modified)
+
+        name2id[directory.name.lower()] = meta.entry_id
+        if meta.english_name:
+            name2id[meta.english_name.lower()] = meta.entry_id
+        if meta.japanese_name:
+            name2id[meta.japanese_name.lower()] = meta.entry_id
+
+    for directory in config.destination.iterdir():
+        try:
+            master_entry = id2master[name2id[directory.name.lower()]]
+        except KeyError:
+            continue
+        if master_entry.dir_path == directory:
+            continue
+        else:
+            print(f"moving '{directory}' to '{master_entry.dir_path}'")
+            move_files(directory, master_entry.dir_path)
+            nuke_dir(directory)
+
+
+def sanitize_directories(config: KitsuConfig) -> None:
+    rename_badly_named_directories(config)
+    merge_directories(config)
