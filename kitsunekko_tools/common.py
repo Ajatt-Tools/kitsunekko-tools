451a1c1fcc28c90005f62025496f8e7160472d6d
Ren Tatsumoto <tatsu@autistici.org>
2024-06-05 16:43:46 +0300

    filter out ' and | in names

--- kitsunekko_tools/common.py
+++ kitsunekko_tools/common.py
@@ -12,7 +12,7 @@ class KitsuException(Exception, abc.ABC):
         raise NotImplementedError()
 
 
-RE_FILENAME_PROHIBITED = re.compile(r"[ _\\\n\t\r#\[\]{}<>^*/:`?]+", flags=re.MULTILINE | re.IGNORECASE)
+RE_FILENAME_PROHIBITED = re.compile(r"[ _\\\n\t\r#\[\]{}<>^*/:`?'|]+", flags=re.MULTILINE | re.IGNORECASE)
 
 
 def fs_name_strip(name: str) -> str:
