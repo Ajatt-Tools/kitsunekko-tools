332d21f6a90e3603ba8f93fc3224afec1a9cee47
Ren Tatsumoto <tatsu@autistici.org>
2024-06-07 01:04:03 +0300

    update format

--- format.sh
+++ format.sh
@@ -5,8 +5,6 @@ echo "Formatting $PWD"
 readarray -t FILES <<<"$(git ls-files | grep -P '\.py$')"
 readonly -a FILES
 
-pyupgrade \
-	--py311-plus \
-	"${FILES[@]}"
-black \
-	"${FILES[@]}"
+pyupgrade --py311-plus "${FILES[@]}"
+isort "${FILES[@]}"
+black "${FILES[@]}"
