1a0f6ba5fb6b9b6be0d61853b3dda13959d5aab7
Ren Tatsumoto <tatsu@autistici.org>
2024-12-10 06:54:33 +0300

    fix sync

--- kitsunekko_tools/__init__.py
+++ kitsunekko_tools/__init__.py
@@ -1,2 +1,5 @@
 # Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+from kitsunekko_tools.scrapper.download import KitsuScrapper
+from kitsunekko_tools.api_access.download import ApiSyncClient
