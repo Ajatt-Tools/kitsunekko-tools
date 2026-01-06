2f43a4d929e5ae9dd661d6ecdfcf18aa89836812
Ren Tatsumoto <tatsu@autistici.org>
2026-01-06 08:35:59 +0300

    run prettier

--- format.sh
+++ format.sh
@@ -2,9 +2,14 @@
 
 echo "Formatting $PWD"
 
+readonly ROOT_DIR=$(git rev-parse --show-toplevel)
+
+cd -- "$ROOT_DIR" || exit 1
+
 readarray -t FILES <<<"$(git ls-files | grep -P '\.py$')"
 readonly -a FILES
 
 pyupgrade --py312-plus "${FILES[@]}"
 isort "${FILES[@]}"
 black "${FILES[@]}"
+prettier -w kitsunekko_tools/example_catalog/resources
