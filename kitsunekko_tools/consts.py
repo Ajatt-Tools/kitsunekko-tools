b0b0546676b0e5764c94a385e071ed7b35fbea18
Ren Tatsumoto <tatsu@autistici.org>
2026-01-06 17:33:27 +0300

    ensure bundled resources exist

--- kitsunekko_tools/consts.py
+++ kitsunekko_tools/consts.py
@@ -12,6 +12,11 @@ TRASH_DIR_NAME = "extra"
 
 THIS_DIR = pathlib.Path(__file__).resolve().parent
 BUNDLED_RESOURCES_DIR = THIS_DIR / "example_catalog" / "resources"
-BUNDLED_TEMPLATES_DIR = THIS_DIR / "example_catalog" / "templates"
+BUNDLED_TEMPLATES_DIR = BUNDLED_RESOURCES_DIR.with_name("templates")
+BUNDLED_SUBTITLES_DIR = BUNDLED_RESOURCES_DIR.with_name("subtitles")
+
+assert BUNDLED_RESOURCES_DIR.is_dir()
+assert BUNDLED_TEMPLATES_DIR.is_dir()
+assert BUNDLED_SUBTITLES_DIR.is_dir()
 
 __all__ = [name for name in globals() if name.isupper()]
