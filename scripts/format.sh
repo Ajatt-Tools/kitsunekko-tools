83e6f2cd3b21f476f53b0ec05b6440f2c7068053
Ren Tatsumoto <tatsu@autistici.org>
2026-04-13 14:48:52 +0300

    move file

--- scripts/format.sh
+++ scripts/format.sh
@@ -9,7 +9,7 @@ cd -- "$ROOT_DIR" || exit 1
 readarray -t FILES <<<"$(git ls-files | grep -P '\.py$')"
 readonly -a FILES
 
-pyupgrade --py312-plus "${FILES[@]}"
+pyupgrade --py313-plus "${FILES[@]}"
 isort "${FILES[@]}"
 black "${FILES[@]}"
 prettier -w kitsunekko_tools/example_catalog/resources
