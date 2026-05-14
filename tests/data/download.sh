66c9530391230663bc186d9de90c968f7f3b5e90
Ren Tatsumoto <tatsu@autistici.org>
2026-05-15 01:11:01 +0300

    add download.sh

---
+++ tests/data/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl 'https://kitsunekko.net/dirlst.php?dir=subtitles%2Fjapanese%2F' -o main_dir_page.html
+curl 'https://kitsunekko.net/dirlst.php?dir=subtitles%2Fjapanese%2FHibike%21+Euphonium%2F' -o subs_page.html
