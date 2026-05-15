c3a3f980b3e6bc16503cc23fd4baac598b254d07
Ren Tatsumoto <tatsu@autistici.org>
2026-05-15 05:24:48 +0300

    refactor consts
    add types

--- kitsunekko_tools/consts.py
+++ kitsunekko_tools/consts.py
@@ -2,21 +2,27 @@
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 import pathlib
+import typing
+from collections.abc import Sequence
 
-PROG_NAME = "kitsunekko-tools"
-SETTINGS_FILE_NAME = f"{PROG_NAME}.toml"
-KITSUNEKKO_DOMAIN_URL = "https://kitsunekko.net"
-IGNORE_FILENAME = ".kitsuignore"
-INFO_FILENAME = ".kitsuinfo.json"
-TRASH_DIR_NAME = "extra"
+S = typing.Final[str]
+P = typing.Final[pathlib.Path]
+SS = typing.Final[Sequence[str]]
 
-THIS_DIR = pathlib.Path(__file__).resolve().parent
-BUNDLED_RESOURCES_DIR = THIS_DIR / "example_catalog" / "resources"
-BUNDLED_TEMPLATES_DIR = BUNDLED_RESOURCES_DIR.with_name("templates")
-BUNDLED_SUBTITLES_DIR = BUNDLED_RESOURCES_DIR.with_name("subtitles")
+PROG_NAME: S = "kitsunekko-tools"
+SETTINGS_FILE_NAME: S = f"{PROG_NAME}.toml"
+KITSUNEKKO_DOMAIN_URL: S = "https://kitsunekko.net"
+IGNORE_FILENAME: S = ".kitsuignore"
+INFO_FILENAME: S = ".kitsuinfo.json"
+TRASH_DIR_NAME: S = "extra"
 
-SUBTITLE_FILE_TYPES = ("srt", "ass", "ssa")
-ARCHIVE_FILE_TYPES = ("zip", "rar", "7z")
+THIS_DIR: P = pathlib.Path(__file__).resolve().parent
+BUNDLED_RESOURCES_DIR: P = THIS_DIR / "example_catalog" / "resources"
+BUNDLED_TEMPLATES_DIR: P = BUNDLED_RESOURCES_DIR.with_name("templates")
+BUNDLED_SUBTITLES_DIR: P = BUNDLED_RESOURCES_DIR.with_name("subtitles")
+
+SUBTITLE_FILE_TYPES: SS = ("srt", "ass", "ssa")
+ARCHIVE_FILE_TYPES: SS = ("zip", "rar", "7z")
 
 assert BUNDLED_RESOURCES_DIR.is_dir()
 assert BUNDLED_TEMPLATES_DIR.is_dir()
@@ -24,9 +30,9 @@ assert BUNDLED_SUBTITLES_DIR.is_dir()
 
 # Default MIME type for files whose extension cannot be guessed.
 # Matches the IANA fallback for "unknown binary" content.
-FALLBACK_MIME_TYPE = "application/octet-stream"
+FALLBACK_MIME_TYPE: S = "application/octet-stream"
 
 # Sitemap namespace used in the sitemap Jinja template.
-SITEMAP_NS = "http://www.sitemaps.org/schemas/sitemap/0.9"
+SITEMAP_NS: S = "http://www.sitemaps.org/schemas/sitemap/0.9"
 
 __all__ = [name for name in globals() if name.isupper()]
