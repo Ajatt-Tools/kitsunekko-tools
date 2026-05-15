e2ec3332ae2c470a05425fc54f64d3c5e5db4b90
Ren Tatsumoto <tatsu@autistici.org>
2026-05-15 03:24:29 +0300

    more verbose output

--- scripts/format.sh
+++ scripts/format.sh
@@ -9,7 +9,11 @@ cd -- "$ROOT_DIR" || exit 1
 readarray -t FILES <<<"$(git ls-files | grep -P '\.py$')"
 readonly -a FILES
 
+echo "Running pyupgrade"
 pyupgrade --py313-plus "${FILES[@]}"
+echo "Running isort"
 isort "${FILES[@]}"
+echo "Running black"
 black "${FILES[@]}"
+echo "Running prettier"
 prettier -w kitsunekko_tools/example_catalog/resources
