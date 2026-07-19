089e0d27bb69faccb7304ff173f745ea5ba0ceba
Ren Tatsumoto <tatsu@autistici.org>
2026-07-19 23:52:07 +0300

    die

--- scripts/test_site_js.sh
+++ scripts/test_site_js.sh
@@ -9,7 +9,7 @@ die() {
 
 main() {
 	[[ -d $PWD/.git ]] || die "started in a wrong directory: $PWD"
-	cd kitsunekko_tools/example_catalog
+	cd -- kitsunekko_tools/example_catalog || die "can't CD to example catalog"
 	pnpm run check:site-js
 	pnpm run test:site-js
         pnpm run coverage:site-js
