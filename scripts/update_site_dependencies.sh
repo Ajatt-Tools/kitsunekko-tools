8b952041cc6d7a8af87e890d9a66b3d375efd3ad
Ren Tatsumoto <tatsu@autistici.org>
2026-08-24 08:51:11 +0300

    add script update_site_dependencies

---
+++ scripts/update_site_dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+ROOT_DIR="$(git rev-parse --show-toplevel)" || exit 1
+readonly ROOT_DIR
+
+die() {
+	echo "oops: $*"
+	exit 1
+}
+
+main() {
+	cd -- "$ROOT_DIR/kitsunekko_tools/example_catalog" || die "can't CD to example catalog"
+	pnpm update --latest
+	pnpm test
+}
+
+main "$@"
